1b5c8c32429ea8a651df9c0ab3612afeed923f6a
dmaceylewis <danna.macey.lewis@gmail.com>
2024-08-14 14:46:10 -0500

    tag repository: insert, update, delete

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -58,6 +58,37 @@ namespace TabloidCLI.UserInterfaceManagers
             }
         }
 
+        private Tag Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Tag:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return tags[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+        }
+
         private void Add()
         {
             Tag tag = new Tag();
@@ -89,7 +120,7 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void Remove()
         {
-            Tag tagToDelete = Choose("Which author would you like to remove?");
+            Tag tagToDelete = Choose("Which tag would you like to remove?");
             if (tagToDelete != null)
             {
                 _tagRepository.Delete(tagToDelete.Id);
